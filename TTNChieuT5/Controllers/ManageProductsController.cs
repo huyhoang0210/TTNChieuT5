ce55c69e30e115857f461c266049ce9617946d9b
Nguyen Huu Nam <nguyenhuunam302@gmail.com>
2019-05-20 16:32:59 +0700

    load data into admin and feature/ add new product

--- TTNChieuT5/Controllers/ManageProductsController.cs
+++ TTNChieuT5/Controllers/ManageProductsController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 using TTNChieuT5.Models;
 using PagedList;
 using PagedList.Mvc;
-
+using System.IO;
 
 namespace TTNChieuT5.Controllers
 {
@@ -16,25 +16,47 @@ namespace TTNChieuT5.Controllers
 
 
         // GET: ManageProducts
-        public ActionResult Index()
+        public ActionResult Index(int ? page)
+        {
+            int pageNumber = page ?? 1;
+            int pageSize = 10;
+            var lst = db.SanPhams.ToList().OrderBy(n=>n.TenSanPham).ToPagedList(pageNumber,pageSize);
+            return View(lst);
+        }
+        public ActionResult Insert()
+        {
+            ViewBag.IDDanhMuc=db.DanhMucSanPhams.ToList();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Insert(SanPham sp,HttpPostedFileBase fileUpLoad)
+        {
+            
+            if (fileUpLoad.ContentLength == 0)
+            {
+                ViewBag.ThongBao = "Bạn chưa chọn ảnh";
+                return View();
+            }
+            if (ModelState.IsValid)
+            {
+                string _fileName = Path.GetFileName(fileUpLoad.FileName);
+                string _path = Path.Combine(Server.MapPath("/Images"), _fileName);
+                fileUpLoad.SaveAs(_path);
+
+                sp.HinhAnh = fileUpLoad.FileName;
+
+                db.SanPhams.Add(sp);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+           
+        }
+        public ActionResult Update(int id)
         {
 
             return View();
-        }
-        public ActionResult PetFoodPartialView()
-        {
-            ViewBag.PetFood = db.SanPhams.Where(n => n.DanhMucSanPham.IDDanhMuc == 1 || n.DanhMucSanPham.IDDanhMuc == 2).ToList();
-            return PartialView();
-        }
-        public ActionResult PetDressPartialView()
-        {
-            var lst = db.SanPhams.Where(n => n.DanhMucSanPham.IDDanhMuc == 3 || n.DanhMucSanPham.IDDanhMuc == 4).ToList();
-            return PartialView(lst);
-        }
-        public ActionResult PetSupPortPartialView()
-        {
-            var lst = db.SanPhams.Where(n => n.DanhMucSanPham.IDDanhMuc == 5 || n.DanhMucSanPham.IDDanhMuc == 6).ToList();
-            return PartialView(lst);
         }
     }
 }
