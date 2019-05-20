6e0efe9ff81806ebb62e5568ddbd766f38313706
= <=>
2019-05-20 16:26:09 +0700

    addLogin

---
+++ TTNChieuT5/Controllers/DangNhapController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TTNChieuT5.Models;
+namespace ThucTapNhom.Controllers
+{
+    public class DangNhapController : Controller
+    {
+        OnlShopDbContext db = new OnlShopDbContext();
+        // GET: DangNhap
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection f)
+        {
+            string tk = Request.Form["TaiKhoan"];
+            string mk = Request.Form["MatKhau"];
+            user taikhoan = db.users.SingleOrDefault(n => n.TaiKhoan == tk && n.MatKhau.Trim() == mk);
+            if (taikhoan != null)
+            {
+                Session["DangNhap"] = taikhoan;
+                return RedirectToAction("Index", "Home");
+
+            }
+            return RedirectToAction("DangNhap", "DangNhap");
+        }
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangKi(FormCollection f)
+        {
+            user TaiKhoan = new user();
+            TaiKhoan.HoTen = Request.Form["HoTen"];
+            TaiKhoan.TaiKhoan = Request.Form["TaiKhoan"];
+            TaiKhoan.MatKhau = Request.Form["MatKhau"];
+            TaiKhoan.SDT = Request.Form["SDT"];
+            TaiKhoan.DiaChi = Request.Form["DiaChi"];
+            db.users.Add(TaiKhoan);
+            db.SaveChanges();
+            return RedirectToAction("DangNhap", "DangNhap");
+        }
+        public ActionResult DangXuat()
+        {
+            Session["DangNhap"] = null;
+            return RedirectToAction("DangNhap", "DangNhap");
+        }
+    }
+}
