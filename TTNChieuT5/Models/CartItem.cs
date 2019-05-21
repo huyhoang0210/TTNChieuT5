85d0ff041871d17f26ec33c7befe73c2a0726bb1
huyhoang0210 <48060295+huyhoang0210@users.noreply.github.com>
2019-05-21 19:16:52 +0700

    Add Cart

---
+++ TTNChieuT5/Models/CartItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TTNChieuT5.Models
+{
+    public class CartItem
+    {
+        OnlShopDbContext db = new OnlShopDbContext();
+
+        public int IdSanPham { get; set; }
+        public string TenSanPham { get; set; }
+        public string HinhAnh { get; set; }
+        public int? SoLuong { get; set; }
+        public int? DonGia { get; set; }
+        public int? ThanhTien { get { return SoLuong * DonGia; } }
+
+        public CartItem(int _idSanPham)
+        {
+            IdSanPham = _idSanPham;
+            SanPham product = db.SanPhams.Find(_idSanPham);
+            TenSanPham = product.TenSanPham;
+            HinhAnh = product.HinhAnh;
+            SoLuong = 1;
+            DonGia = product.Gia;
+        }
+        public CartItem(int _idSanPham, int _SoLuong)
+        {
+            IdSanPham = _idSanPham;
+            SanPham product = db.SanPhams.Find(_idSanPham);
+            TenSanPham = product.TenSanPham;
+            HinhAnh = product.HinhAnh;
+            SoLuong = _SoLuong;
+            DonGia = product.Gia;
+        }
+    }
+}
